1bcc08abe0a83fd921df30bfcb477d589f184caf
daviddalpiaz <dalpiaz2@illinois.edu>
2020-10-24 13:29:44 -0500

    init with data

---
+++ make-data.R
@@ -0,0 +1,25 @@
+# extract and read zipped csv file obtained from Kaggle
+# https://www.kaggle.com/mlg-ulb/creditcardfraud
+creditcard = data.table::fread('unzip -p data-raw/creditcard.csv.zip')
+
+# make response a factor with names instead of numbers
+creditcard$Class = factor(ifelse(creditcard$Class == 0, "genuine", "fraud"))
+
+# create remote readable compressed file
+data.table::fwrite(creditcard, file = "data/creditcard.csv.gz")
+
+# read from gz file with modified response
+cc = data.table::fread("data/creditcard.csv.gz")
+
+# verify data
+nrow(creditcard) == 284807
+nrow(cc) == 284807
+
+# create data subset
+set.seed(42)
+sub_idx = sample(nrow(cc), size = 10000)
+cc_sub = cc[sub_idx, ]
+
+# write subset to disk
+data.table::fwrite(cc_sub, "data/cc-sub.csv")
+
